3795a947cb14f8b438bd5f94f38c93717c6e5a99
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2021-12-08 21:15:38 +0100

    add first working version

--- main.py
+++ main.py
@@ -1,65 +1,88 @@
 import os
 import random
 
+WHOLE_GROUP = ['Kornél', 'Nelli', 'Balázs', 'Nándi', 'Évi', 'Bence', 'Barbi', 'Orsi', "Gee"]
+
 def screen_clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# WHOLE_GROUP = {1:'Kornél', 2:'Nelli', 3:'Balázs', 4:'Nándi', 5:'Évi', 6:'Bence', 7:'Barbi', 8:'Orsi', 9:"Gee"}
-WHOLE_GROUP = {'Kornél', 'Nelli', 'Balázs', 'Nándi', 'Évi', 'Bence', 'Barbi', 'Orsi', "Gee"}
+
+def choose_try_again():
+    try_again = input('Do you want to try again? \n 1 - Yes \n 2 - No \n')
+    if try_again == '1':
+        screen_clear()
+        main()
+    elif try_again == '2':
+        quit()
+    else:
+        print('Please provide a valid input!')
+        choose_try_again()
+
+
+def print_groups(final_group):
+    print('The members for the groups are:')
+    for count, members in enumerate(final_group):
+        print(f'Group {count + 1} : {members}')
+
+
+def get_groups(group, number_of_group_members, number_of_groups):
+    final_group = [[ '' for j in range(int(number_of_group_members))] for i in range(number_of_groups)]
+    shuffled_group = list(group)
+    random.shuffle(shuffled_group)
+    group_index = [i for i in range(len(group))]
+    for i in range(number_of_groups):
+        for j in range(int(number_of_group_members)):
+            index_num = random.choice(group_index)
+            group_index.pop(group_index.index(index_num))
+            final_group[i][j] = shuffled_group[index_num]
+    if len(group) - number_of_groups * number_of_group_members > number_of_groups:
+        final_group.append([])
+        for count in range(len((group_index))):
+            index_num = random.choice(group_index)
+            group_index.pop(group_index.index(index_num))
+            final_group[number_of_groups].append(shuffled_group[index_num])
+    if len(group_index) != 0:
+        for count in range(len((group_index))):
+            index_num = random.choice(group_index)
+            group_index.pop(group_index.index(index_num))
+            final_group[count].append(shuffled_group[index_num])
+    return final_group
 
 
 def get_number_of_group_members(group):
-    number_of_group_members = input('How many people should be in a group? \n')
+    number_of_group_members = int(input('How many people should be in a group? \n'))
     number_of_groups = len(group) // number_of_group_members
-    
-
-
+    if number_of_groups == 0:
+        print('Please provide a valid input')
+        get_number_of_group_members(group)
+    return number_of_group_members, number_of_groups
+   
+ 
 def add_members(group):
     screen_clear()
     new_member = input('Please provide the name of the new member! \n')
-    group[len(group) + 1] = new_member
-    screen_clear()
-    print(f'Current members are: {get_group(group)}')
-    do_it_again = input('Do you want to add more members? \n 1 - Yes \n 2 - No \n 3 - Other change \n')
-    if do_it_again == '1':
-        add_members(group)
-    elif do_it_again == '2':
-        return group
-    elif do_it_again == '3':
-        choose_custom_members(group)
-
+    group.append(new_member)
+    choose_custom_members(group)
+  
 
 def delete_members(group):
-    screen_clear()
-    print(f'{group}')
-    remove_member = input('Please choose the member\'s number to remove the member! \n')
-    del group[int(remove_member)]
-    print(f'{group}')
-    do_it_again = input('Do you want to delete more members? \n 1 - Yes \n 2 - No \n 3 - Other change \n')
-    if do_it_again == '1':
+    group_dict = get_group_dict_from_list(group)
+    print(f'{group_dict}')
+    remove_member = int(input('Please choose the member\'s number to remove the member! \n'))
+    if remove_member not in group_dict.keys():
+        print('Provide a valid input!')
         delete_members(group)
-    elif do_it_again == '2':
-        return group
-    elif do_it_again == '3':
-        choose_custom_members(group)
-
-
-def create_new_list(group):
-    screen_clear()
-    new_member = input('Please give the members\'s name! \n') 
-    group[len(group + 1)] = new_member
-    do_it_again = input('Do you want to add more members? \n 1 - Yes \n 2 - No \n 3 - Other change \n')
-    if do_it_again == '1':
-        create_new_list(group)
-    elif do_it_again == '2':
-        return group
-    elif do_it_again == '3':
-        choose_custom_members(group)
-
+    del group_dict[int(remove_member)]
+    group = get_group_list_from_dict(group_dict)
+    choose_custom_members(group)
+  
 
 def choose_custom_members(group):
+    group = list(group)
     screen_clear()
-    method = input('''
+    method = input(f'''
+The current members are: {group}
+
 Do you want to add or delete a member or create a new list?
     
 1 - Add
@@ -69,32 +92,42 @@ Do you want to add or delete a member or create a new list?
     
 ''')
     if method == '1':
+        screen_clear()
         add_members(group)
     elif method == '2':
+        screen_clear()
         delete_members(group)
     elif method == '3':
-        group = {}
-        create_new_list(group)
+        screen_clear()
+        group = []
+        add_members(group)
     elif method == '4':
-        get_number_of_group_members(group)
+        return group
     else:
         print('Please provide a valid input! \n')
         choose_custom_members(group)
+    return group
 
 
-def get_group(WHOLE_GROUP):
+def get_group_list_from_dict(group):
     temp_group = []
-    for key, value in WHOLE_GROUP.items():
+    for key, value in group.items():
         temp_group.append(value)
     return temp_group
+
+
+def get_group_dict_from_list(group):
+    group_list = {}
+    for i in group:
+       group_list[len(group_list) + 1] = i
+    return group_list
     
 
-def get_names():
-    group_list = get_group(WHOLE_GROUP)
+def choose_group():
     group = WHOLE_GROUP
     names = input(f'''
 Please provide the names to make the groups of!
-1 - Whole group - {group_list}
+1 - Whole group - {group}
 2 - Custom members
     
 ''')
@@ -104,17 +137,23 @@ Please provide the names to make the groups of!
         choose_custom_members(group)
     else:
         print('Please provide a valid input! \n')
-        get_names()
+        choose_group()
+    return group
 
 
 def welcome():
-    print('welcome to the Random Group Creator! \n')
+    print('Welcome to the Random Group Creator! \n')
 
 
-def main():
+def main(group=None):
     welcome()
-    group = get_names()
-
+    group = choose_group()
+    screen_clear()
+    number_of_group_members, number_of_groups = get_number_of_group_members(group)
+    final_groups = get_groups(group, number_of_group_members, number_of_groups)
+    screen_clear()
+    print_groups(final_groups)
+    choose_try_again()
 
 if __name__ == '__main__':
-    main()
+    main(group=None)
